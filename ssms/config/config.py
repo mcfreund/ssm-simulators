e18abf0bcbbbb08477664d4fc33ae439b5d766da
Carlos Paniagua <cpaniaguam@gmail.com>
2025-04-25 00:06:02 -0400

    Modularize levy model config (#128)
    Co-authored-by: Alexander Fengler <alexanderfengler@gmx.de>

--- ssms/config/config.py
+++ ssms/config/config.py
@@ -23,6 +23,7 @@ import scipy.stats as sps  # type: ignore
 from ssms.basic_simulators import boundary_functions as bf
 from ssms.basic_simulators import drift_functions as df
 
+from ssms.config._modelconfig.levy import get_levy_config, get_levy_angle_config
 from ._modelconfig.angle import get_angle_config
 from ._modelconfig.weibull import get_weibull_config
 
@@ -125,37 +126,10 @@ model_config = {
         "n_particles": 1,
         "simulator": cssm.ddm,
     },
+    "levy": get_levy_config(),
+    "levy_angle": get_levy_angle_config(),
     "angle": get_angle_config(),
     "weibull": get_weibull_config(),
-    "levy": {
-        "name": "levy",
-        "params": ["v", "a", "z", "alpha", "t"],
-        "param_bounds": [[-3.0, 0.3, 0.1, 1.0, 1e-3], [3.0, 3.0, 0.9, 2.0, 2]],
-        "boundary_name": "constant",
-        "boundary": bf.constant,
-        "n_params": 5,
-        "default_params": [0.0, 1.0, 0.5, 1.5, 0.1],
-        "nchoices": 2,
-        "choices": [-1, 1],
-        "n_particles": 1,
-        "simulator": cssm.levy_flexbound,
-    },
-    "levy_angle": {
-        "name": "levy_angle",
-        "params": ["v", "a", "z", "alpha", "t", "theta"],
-        "param_bounds": [
-            [-3.0, 0.3, 0.1, 1.0, 1e-3, -0.1],
-            [3.0, 3.0, 0.9, 2.0, 2, 1.3],
-        ],
-        "boundary_name": "angle",
-        "boundary": bf.angle,
-        "n_params": 6,
-        "default_params": [0.0, 1.0, 0.5, 1.5, 0.1, 0.01],
-        "nchoices": 2,
-        "choices": [-1, 1],
-        "n_particles": 1,
-        "simulator": cssm.levy_flexbound,
-    },
     "full_ddm": {
         "name": "full_ddm",
         "params": ["v", "a", "z", "t", "sz", "sv", "st"],
