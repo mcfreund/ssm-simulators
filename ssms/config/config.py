e474a56841a23a07285b7acbcde7d1de940cf427
Carlos Paniagua <cpaniaguam@gmail.com>
2025-04-24 23:50:12 -0400

    Modularize angle model config (#126)
    Co-authored-by: Alexander Fengler <alexanderfengler@gmx.de>

--- ssms/config/config.py
+++ ssms/config/config.py
@@ -23,6 +23,7 @@ import scipy.stats as sps  # type: ignore
 from ssms.basic_simulators import boundary_functions as bf
 from ssms.basic_simulators import drift_functions as df
 
+from ._modelconfig.angle import get_angle_config
 from ._modelconfig.weibull import get_weibull_config
 
 
@@ -124,19 +125,7 @@ model_config = {
         "n_particles": 1,
         "simulator": cssm.ddm,
     },
-    "angle": {
-        "name": "angle",
-        "params": ["v", "a", "z", "t", "theta"],
-        "param_bounds": [[-3.0, 0.3, 0.1, 1e-3, -0.1], [3.0, 3.0, 0.9, 2.0, 1.3]],
-        "boundary_name": "angle",
-        "boundary": bf.angle,
-        "n_params": 5,
-        "default_params": [0.0, 1.0, 0.5, 1e-3, 0.0],
-        "nchoices": 2,
-        "choices": [-1, 1],
-        "n_particles": 1,
-        "simulator": cssm.ddm_flexbound,
-    },
+    "angle": get_angle_config(),
     "weibull": get_weibull_config(),
     "levy": {
         "name": "levy",
