70cecde182c4803f694fc1ed2da6830a37994cf7
Carlos Paniagua <cpaniaguam@gmail.com>
2025-08-25 20:03:04 +0000

    Fix typo in parameterize decorator for model_config in test_data_generator.py

--- tests/data_generator/test_data_generator.py
+++ tests/data_generator/test_data_generator.py
@@ -77,7 +77,7 @@ slow_prefixes = (
 )
 
 
-@pytest.mark.parametrize("model_name,model_conf", model_config.item())
+@pytest.mark.parametrize("model_name,model_conf", model_config.items())
 def test_data_generator(model_name, model_conf):
     if model_name in broken_models:
         pytest.skip(f"Skipping broken model: {model_name}")
