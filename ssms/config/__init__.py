991de10def610e501857381fbdbfd0029b31508a
Carlos Paniagua <cpaniaguam@gmail.com>
2025-09-10 17:07:53 -0400

    Refactor generator configuration imports for improved clarity and organization

--- ssms/config/__init__.py
+++ ssms/config/__init__.py
@@ -19,15 +19,8 @@ from .generator_config.data_generator_config import (
     get_kde_simulation_filters,
     get_lan_config,
     get_opn_only_config,
-    get_cpn_only_config,
-    get_kde_simulation_filters,
-    get_defective_detector_config,
     get_ratio_estimator_config,
-    get_default_generator_config,
-    data_generator_config,  # TODO: remove from interface in v1.0.0
 )
-
-from ._modelconfig.base import boundary_config, drift_config
 from .kde_constants import KDE_NO_DISPLACE_T  # noqa: F401
 
 
