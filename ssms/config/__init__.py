6d3b9dafc28803b723c2dc31c6f38f794b65ded8
Carlos Paniagua <cpaniaguam@gmail.com>
2025-09-10 17:07:03 -0400

    Refactor import statements in configuration module for clarity and organization

--- ssms/config/__init__.py
+++ ssms/config/__init__.py
@@ -6,12 +6,17 @@ It centralizes all configuration-related functionality to ensure consistent
 parameter settings across simulations.
 """
 
-from .config import (
-    boundary_config_to_function_params,
-    model_config,
-)
+import copy
 
+from ssms.config._modelconfig import get_model_config
+
+from ._modelconfig.base import boundary_config, drift_config
 from .generator_config.data_generator_config import (
+    data_generator_config,  # TODO: remove from interface in v1.0.0
+    get_cpn_only_config,
+    get_default_generator_config,
+    get_defective_detector_config,
+    get_kde_simulation_filters,
     get_lan_config,
     get_opn_only_config,
     get_cpn_only_config,
