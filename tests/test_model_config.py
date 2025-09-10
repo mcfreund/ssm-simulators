eaa62cee759b2d9fe12c7b1f5e34dc5b3cf591d3
Carlos Paniagua <cpaniaguam@gmail.com>
2025-09-10 17:15:41 -0400

    Refactor test for model configuration to use random model selection

--- tests/test_model_config.py
+++ tests/test_model_config.py
@@ -1,12 +1,14 @@
+import random
 import ssms
 from ssms.config import model_config
+
 
 class TestModelConfig:
     def test_model_config_dict_type(self):
         assert isinstance(model_config, ssms.config.CopyOnAccessDict)
 
-
     def test_model_config_copy_on_access(self):
-        list_params = model_config["shrink_spot_simple_extended"]["params"]
+        model_name = random.choice(list(model_config.keys()))
+        list_params = model_config[model_name]["params"]
         list_params.append("p_outlier")
-        assert "p_outlier" not in model_config["shrink_spot_simple_extended"]["params"]
+        assert "p_outlier" not in model_config[model_name]["params"]
