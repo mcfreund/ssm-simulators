36b27bcbde1d0ce678fecd19cd97597abe0194be
Carlos Paniagua <cpaniaguam@gmail.com>
2025-09-12 09:24:49 -0400

    Refactor test for model_config to improve clarity

--- tests/test_model_config.py
+++ tests/test_model_config.py
@@ -8,7 +8,8 @@ class TestModelConfig:
         assert isinstance(model_config, ssms.config.CopyOnAccessDict)
 
     def test_model_config_copy_on_access(self):
-        model_name = random.choice(list(model_config.keys()))
-        list_params = model_config[model_name]["params"]
+        model_name = random.choice(list(model_config))
+        selected_model = model_config[model_name]
+        list_params = selected_model["params"]
         list_params.append("p_outlier")
         assert "p_outlier" not in model_config[model_name]["params"]
