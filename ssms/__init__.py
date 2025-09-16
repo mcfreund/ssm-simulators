8bab31366e66bbd97a4fc946266ad8abf7873437
Carlos Paniagua <cpaniaguam@gmail.com>
2025-09-16 12:58:55 -0400

    Add hssm_support to imports and __all__ in __init__.py

--- ssms/__init__.py
+++ ssms/__init__.py
@@ -1,5 +1,5 @@
-from . import basic_simulators, config, dataset_generators, support_utils  # noqa: D104
+from . import basic_simulators, config, dataset_generators, support_utils, hssm_support  # noqa: D104
 
 __version__ = "0.9.0"  # importlib.metadata.version(__package__ or __name__)
 
-__all__ = ["basic_simulators", "dataset_generators", "config", "support_utils"]
+__all__ = ["basic_simulators", "dataset_generators", "config", "support_utils", "hssm_support"]
